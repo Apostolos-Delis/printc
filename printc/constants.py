ee4d183464c2b07a065f8706ccb9064d49b08ce3
Apostolos-Delis <apost.delis@gmail.com>
2019-08-21 22:11:27 -0700

    initialized constants.py

--- printc/constants.py
+++ printc/constants.py
@@ -5,8 +5,19 @@
 constants.py
 Date: 08-21-2019
 
-Description:
+Description: Defines the codes required for ansi code generation
 """
+
+
+COLOR_CODES = {
+    "b": "blue",
+    "r": "red",
+}
+
+COLORS = {
+    "black": ("30m", "40;"),
+    "red": ("31m", "41;"),
+}
 
 
 if __name__ == "__main__":
